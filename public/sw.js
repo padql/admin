ce84d46f91a5d4fc4edf6c22ada1731722461b0a
Ihfadzh D <ihfadzhdiinaka7@gmail.com>
2025-10-10 22:41:33 +0700

    feat: add service worker for caching and offline support

---
+++ public/sw.js
@@ -0,0 +1,50 @@
+const CACHE_NAME = "adminhub-v1";
+const URLS_TO_CACHE = [
+  "/",
+  "/index.html",
+  "/manifest.json",
+  "/favicon.ico",
+];
+
+// Install dan cache file
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(URLS_TO_CACHE);
+    })
+  );
+  self.skipWaiting();
+});
+
+// Hapus cache lama saat update
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((names) =>
+      Promise.all(
+        names.map((name) => {
+          if (name !== CACHE_NAME) {
+            return caches.delete(name);
+          }
+        })
+      )
+    )
+  );
+  self.clients.claim();
+});
+
+// Ambil dari cache dulu baru dari network
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cachedResponse) => {
+      return (
+        cachedResponse ||
+        fetch(event.request).then((response) => {
+          return caches.open(CACHE_NAME).then((cache) => {
+            cache.put(event.request, response.clone());
+            return response;
+          });
+        })
+      );
+    })
+  );
+});
